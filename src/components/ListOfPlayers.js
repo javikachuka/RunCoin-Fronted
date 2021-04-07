5baa959f1a07717dc0a55434b4b0b98dba5e0ed2
Javier <kachuka99@gmail.com>
2021-04-06 23:08:29 -0300

    listen to blockchain changed

--- src/components/ListOfPlayers.js
+++ src/components/ListOfPlayers.js
@@ -10,62 +10,16 @@ import Loading from './Loading';
 
 const ListOfPlayers = () => {
 
-    const { list, setNewGame, load } = useList()
+    // const { list, setNewGame, load } = useList()
 
-
-
-
-    // const [list, setList] = useState([])
+    const [list, setList] = useState([])
+    const [load, setLoad] = useState(true)
     // const [newGame, setNewGame] = useState(null)
-
-    // useEffect(
-    //     () => {
-    //         const response = miContrato.events.Game(
-    //             {
-    //                 // filter: {myIndexedParam: [20,23], myOtherIndexedParam: '0x123456789...'},
-    //                 fromBlock: 'latest'
-    //             }
-    //             // , (error, event) => {
-    //             //     const { listado, setListado } = useList()
-    //             //     console.log(listado);
-    //             //     console.log('Evento activado');
-    //             //     console.log(event);
-    //             //     let newGame = {
-    //             //         player: event.returnValues.owner_play,
-    //             //         timeGame: 0,
-    //             //         timestamp: event.returnValues.timestamp,
-    //             //         wait: event.returnValues.wait
-    //             //     }
-    //             //     return newGame
-    //             //     // setList([...list, newGame])
-    //             // }
-    //         )
-    //         .on('data', 
-    //             // res => actualizarList(res)
-    //             res => {
-    //                 console.log(res);
-    //                 console.log(list);
-    //                 // console.log('Evento activado');
-    //                 // console.log(res);
-    //                 let newGame = {
-    //                     player: res.returnValues.owner_play,
-    //                     timeGame: 0,
-    //                     timestamp: res.returnValues.timestamp,
-    //                     wait: res.returnValues.wait
-    //                 }
-    //                 return newGame
-    //                 // setList([...list, newGame])
-    //             }
-    //         )
-    //         console.log(response);
-    //         console.log('saliendo');
-    //     }, []
-    // )
 
 
     useEffect(
         () => {
-
+            fetchApi()
             miContrato.events.Game(
                 {
                     // filter: {myIndexedParam: [20,23], myOtherIndexedParam: '0x123456789...'},
@@ -73,48 +27,38 @@ const ListOfPlayers = () => {
                 }
                 , (error, event) => {
                     console.log('Evento activado');
-                    let newGame = {
-                        player: event.returnValues.owner_play,
-                        timeGame: 0,
-                        timestamp: event.returnValues.timestamp,
-                        wait: event.returnValues.wait
-                    }
-                    // return newGame
-                    setNewGame(newGame)
-                    console.log(newGame);
+                    setLoad(true)
+                    fetchApi()
                 }
             )
-            // setList([newGame, ...list])
-
             console.log('saliendo');
         }, [] 
     )
 
-    // async function fetchApi() {
-    //     await listPlayerLastSeassons(80)
-    //         .then(
-    //             (result) => {
-    //                 var array = result.map(
-    //                     (r) => {
-    //                         return {
-    //                             ...r,
-    //                             player: r.player,
-    //                             timeGame: r.timeGame,
-    //                             timestamp: r.timestamp,
-    //                             wait: r.wait,
-    //                         }
-    //                     }
-    //                 )
-    //                 console.log(array);
-    //                 setList(array);
-
-    //             }
-    //         ).catch(
-    //             (error) => {
-    //                 console.log(error)
-    //             }
-    //         )
-    // }
+    async function fetchApi() {
+        await listPlayerLastSeassons(9)
+            .then(
+                (result) => {
+                    var array = result.map(
+                        (r) => {
+                            return {
+                                ...r,
+                                player: r.player,
+                                timeGame: r.timeGame,
+                                timestamp: r.timestamp,
+                                wait: r.wait,
+                            }
+                        }
+                    )
+                    setList(array);
+                    setLoad(false)
+                }
+            ).catch(
+                (error) => {
+                    console.log(error)
+                }
+            )
+    }
 
     return (
         <>
