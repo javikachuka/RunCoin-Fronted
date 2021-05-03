54118b0581fff51a69760581898e57526d49f4af
Javier <kachuka99@gmail.com>
2021-05-03 11:04:03 -0300

    button clain

---
+++ src/components/Game/ButtonClaim/ButtonClaim.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { PlayButton } from '../CurrentGame/CurrentGame.elements';
+
+const ButtonClaim = () => {
+    return ( 
+        <PlayButton  className="claim">
+            CLAIM
+        </PlayButton>
+     );
+}
+ 
+export default ButtonClaim;
