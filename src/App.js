e84de96b794ece75da5b83e9c1d64e43d404f49e
Javier <kachuka99@gmail.com>
2021-06-09 22:42:53 -0300

    changes in routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
-import { Route } from "wouter";
+import { Redirect, Route } from "wouter";
 import Game from "./pages/Game";
 import { LoginContextProvider } from "./context/LoginContext";
 import { ListContextProvider } from "./context/ListContext";
-import {BarContextProvider} from './context/BarContext'
+import { BarContextProvider } from './context/BarContext'
 import * as We from "./services/server";
 import Landing from "./pages/Landing";
 import GlobalStyle from "./globalStyles";
@@ -14,18 +14,21 @@ const App = () => {
   return (
     <>
       <GlobalStyle />
-      <Route path="/">
+      <Route exact path="/">
+        <Redirect to="/game" />
+      </Route>
+      <Route path="/home">
         <Landing />
         {/* <h1>Hola esta funcionando</h1> */}
       </Route>
-      <Route path="/game">
+      <Route path="/game2">
         <LoginContextProvider>
           <ListContextProvider>
             <Game />
           </ListContextProvider>
         </LoginContextProvider>
       </Route>
-      <Route path="/game2">
+      <Route path="/game">
         <LoginContextProvider>
           <ListContextProvider>
             <BarContextProvider>
