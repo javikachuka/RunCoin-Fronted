9c3cbbd0edf11a21f2e62e7835730df245ab4d74
Agus <agus.britez@hotmail.com>
2021-06-14 17:17:24 -0300

    casi ultimo contrato arreglos de codigo

--- src/services/parameters.js
+++ src/services/parameters.js
@@ -6,7 +6,7 @@ export let addressContractR='0x45B051108F46CD9E76360CF90aD395403786DC23' /* ulti
 export let TG_ContractR='0x278F8686ca6B3455cDc7Add34Ed114aD59a1bD62'  
 
 
-export let addressContractOKT='0x6a695ed48D8dae2a96d23a6Dc4A95e36f28CA446';//red OKT
+export let addressContractOKT='0xc82c493E35b8f9ffc260a6CD0efc47971Ba3bea9';//red OKT
 export let TG_ContratOKT='0xfba3cd0ad727e834d9890c1a1761c39cb1b3a41d';
 // export let addressContractR2='0xd457A1e1EDB98566f70507f1e7c601472A12A04B'
 // export let addressContractR3='0x7467ee6CA62DAB2bAdC7198A011f22D27Bc913e2'
